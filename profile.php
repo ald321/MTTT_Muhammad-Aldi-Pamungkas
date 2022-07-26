fbc3c24caa28d69d5dd03b3da187b71c4f05a45c
Muhammad Aldi Pamungkas <109944822+ald321@users.noreply.github.com>
2022-07-27 04:48:17 +0700

    update

--- profile.php
+++ profile.php
@@ -9,16 +9,24 @@ exit;
 
 
 include "FH/header.php";
+$id = $_SESSION["profile"]["id"];
+$username = $_SESSION["profile"]["username"]
+include "App/class_akun.php";
+
+  $mysqli = new koneksi();
+
+ 
 
 ?>
 
                     
-                    <label><?php echo $_SESSION["user"]["username"]; ?></label>
-                    <label><?php echo $_SESSION["profile"]["email"]; ?></label>
+                  
 <section class="bg-light">
     <div class="container">
         <div class="row">
             <div class="col-lg-12 mb-4 mb-sm-5">
+<?php $data = "SELECT * FROM user_profile WHERE id = '$id' ORDER BY id DESC "; ?> 
+<?php foreach (mysqli_query($mysqli->mysqli_akun, $data) as $show) { ?>
                 <div class="card card-style1 border-0">
                     <div class="card-body p-1-9 p-sm-2-3 p-md-6 p-lg-7">
                         <div class="row align-items-center">
@@ -28,29 +36,35 @@ include "FH/header.php";
                                     <h3 class="h2 text-white mb-0">Profile</h3>
                                     
                                 </div>
+
                                 <br>
                                 <br>
                                 <div>
+                                  <a href="profile_ubah.php?id=<?= $show["id"];  ?>" class="btn btn-warning"> Ubah Profile </a>
                               <h5>Nama</h5>
-                              <input class="form-control form-control-lg" id="nama" type="text" name="nama" required="" placeholder="<?php echo $_SESSION["profile"]["nama"]; ?>" autocomplete="off" disabled="" >
+                              <input class="form-control form-control-lg" id="nama" type="text" name="nama" required="" placeholder="<?= $show["nama"]; ?>" autocomplete="off" disabled="" >
                           </div>
                           <div>
                           <h5>Alamat</h5>
-                            <textarea class="form-control form-control-lg" id="nama" type="text" name="nama" required="" placeholder="<?php echo $_SESSION["profile"]["alamat"]; ?>" autocomplete="off" disabled="" ></textarea> 
+                            <textarea class="form-control form-control-lg" id="nama" type="text" name="nama" required="" placeholder="<?= $show["alamat"]; ?>" autocomplete="off" disabled="" ></textarea> 
                           </div>
                            <div>
                           <h5>Tanggal Lahir</h5>
-                              <input class="form-control form-control-lg" id="nama" type="text" name="nama" required="" placeholder="<?php echo $_SESSION["profile"]["tanggal_lahir"]; ?>" autocomplete="off" disabled="" >
+                              <input class="form-control form-control-lg" id="nama" type="text" name="nama" required="" placeholder="<?= $show["tanggal_lahir"]; ?>" autocomplete="off" disabled="" >
                           </div>
                            <div>
                           <h5>Email</h5>
-                              <input class="form-control form-control-lg" id="nama" type="text" name="nama" required="" placeholder="<?php echo $_SESSION["profile"]["email"]; ?>" autocomplete="off" disabled="" >
+                              <input class="form-control form-control-lg" id="nama" type="text" name="nama" required="" placeholder="<?= $show["email"]; ?>" autocomplete="off" disabled="" >
                           </div>
 
                             </div>
                         </div>
                     </div>
                 </div>
+                <?php } ?>
+                <br>
+
+  <a href="user_skill_tambah.php?id=<?= $id; ?>" class="btn btn-primary"> Tambah Skill </a>
             </div>
             <div class="col-lg-12 mb-4 mb-sm-5">
                 <table class="table">
@@ -64,9 +78,13 @@ include "FH/header.php";
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
+   
+      <th></th>
+      <td></td>
+      <td>
+        <a href="user_skill_ubah.php?id=<?= $id; ?>" class="btn btn-warning"> Ubah </a>
+         <a href="user_skill_hapus.php?id=<?= $id; ?>" class="btn btn-danger" onclick="return confirm(' Yakin Ingin Hapus?');"> Hapus </a>
+      </td>
       
     </tr>
    
